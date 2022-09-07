d2e27d7b2ba9e6005e932df81c2d13ec2d818124
apisceva <aljona.gromova@gmail.com>
2022-09-07 09:34:59 -0400

    added SpeakerContext and some changes in Speaker and SpeakerList components

--- src/components/SpeakersList.js
+++ src/components/SpeakersList.js
@@ -1,16 +1,19 @@
+import { useContext } from "react";
 import Speaker from "./Speaker";
 import ReactPlaceholder from "react-placeholder";
 import useRequestDelay, { REQUEST_STATUS } from "../hooks/useRequestDelay";
 import { data } from "../SpeakerData";
 import { SpeakerFilterContext } from "../contexts/SpeakerFilterContext";
 
-function SpeakersList() {
+function SpeakersList({ showSessions }) {
   const {
     data: speakersData,
     requestStatus,
     error,
     updateRecord,
   } = useRequestDelay(2000, data);
+
+  const { searchQuery, eventYear } = useContext(SpeakerFilterContext);
 
   if (requestStatus === REQUEST_STATUS.FAILURE) {
     return (
@@ -29,23 +32,27 @@ function SpeakersList() {
         ready={requestStatus === REQUEST_STATUS.SUCCESS}
       >
         <div className="row">
-          {data.map(function (speaker) {
-            return (
-              <Speaker
-                key={speaker.id}
-                speaker={speaker}
-                onFavoriteToggle={(doneCallback) => {
-                  updateRecord(
-                    {
-                      ...speaker,
-                      favorite: !speaker.favorite,
-                    },
-                    doneCallback
-                  );
-                }}
-              />
-            );
-          })}
+          {speakersData
+            .filter(function (speaker) {
+              return (
+                speaker.first.toLowerCase().includes(searchQuery) ||
+                speaker.last.toLowerCase().includes(searchQuery)
+              );
+            })
+            .filter(function (speaker) {
+              return speaker.sessions.find((session) => {
+                return session.eventYear === eventYear;
+              });
+            })
+            .map(function (speaker) {
+              return (
+                <Speaker
+                  key={speaker.id}
+                  speaker={speaker}
+                  updateRecord={updateRecord}
+                />
+              );
+            })}
         </div>
       </ReactPlaceholder>
     </div>
