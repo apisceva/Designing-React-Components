66782b08fe29bc4c15c5dcb0c9453932af73ea57
apisceva <aljona.gromova@gmail.com>
2022-09-13 04:00:36 -0400

    some updates in Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,31 +2,9 @@ import { ThemeContext } from "../contexts/ThemeContext";
 import { useContext } from "react";
 import withAuth from "./withAuth";
 
-function Header({loggedInUser, setLoggedInUser}) {
-  const { theme } = useContext(ThemeContext); // {setTheme, theme} = value
+function Header({ loggedInUser, setLoggedInUser }) {
+  const { theme } = useContext(ThemeContext);
 
-  function LoggedIn({loggedInUser, setLoggedInUser}) {
-    return (
-      <div>
-        <span>Logged in as {loggedInUser}</span>
-        <button className="btn btn-secondary"
-        onClick={() => {
-          setLoggedInUser("");
-        }}>Logout</button>
-      </div>
-    )
-  }
-
-  function NotLoggedIn({loggedInUser, setLoggedInUser}) {
-    return (
-      <button className="btn-secondary"
-      onClick={(e) => {
-        e.preventDefault();
-        const username = window.prompt("Enter Login Name:", "");
-        setLoggedInUser(username);
-      }}>Login</button>
-    )
-  }
   return (
     <div className="padT4 padB4">
       <div className="container mobile-container">
@@ -38,10 +16,30 @@ function Header({loggedInUser, setLoggedInUser}) {
             <h4 className="header-title">Silicon Valley Code Camp</h4>
           </div>
           <div className={theme === "light" ? "" : "text-info"}>
-            {loggedInUser && loggedInUser.length > 0 ?
-            <LoggedIn loggedInUser={loggedInUser} setLoggedInUser={setLoggedInUser} />
-          <NotLoggedIn loggedInUser={loggedInUser} setLoggedInUser={setLoggedInUser} />
-        }
+            {loggedInUser && loggedInUser.length > 0 ? (
+              <div>
+                <span>Logged in as {loggedInUser} </span>
+                <button
+                  className="btn btn-secondary"
+                  onClick={() => {
+                    setLoggedInUser("");
+                  }}
+                >
+                  Logout
+                </button>
+              </div>
+            ) : (
+              <button
+                className="btn btn-secondary"
+                onClick={(e) => {
+                  e.preventDefault();
+                  const username = window.prompt("Enter Login name:", "");
+                  setLoggedInUser(username);
+                }}
+              >
+                login
+              </button>
+            )}
           </div>
         </div>
       </div>
